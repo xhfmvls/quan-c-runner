aca51b3ea58733de689f77d24b2d2bda1d741c3b
unknown <vincent.pradipta@binus.ac.id>
2024-05-27 00:10:20 +0700

    UPDATE: Update code to be able tun run asyncronously

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import signal
-from fastapi import FastAPI, HTTPException, File, UploadFile, Form
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, BackgroundTasks
 from python_on_whales import DockerClient
 import docker
 import asyncio
@@ -15,7 +15,7 @@ TODO:
 - ✅ buat mekanisme update app.js / main.py / dll (yang gw kepikiran sekarang kita buat .txt yang isinya nama file yang perlu diganti (pake txt aja biar kita nga perlu buat kolom baru di ERD :) ))
 - jalanin testing payload
 - ngitung performance (waktu build n testing)
-- buat async sehingga bisa sekaligus beberapa (ini rada low prioritynya, karna yang penting adalah functionallity nya jalan dulu)
+- ✅ buat async sehingga bisa sekaligus beberapa (ini rada low prioritynya, karna yang penting adalah functionallity nya jalan dulu)
 '''
 
 class Submission(BaseModel):
@@ -52,22 +52,58 @@ def get_dir_data(file_path):
         print(f"File not found: {file_path}")
         return None, None
 
+def build_and_run_docker(challenge_id: str, submission_id: str, destination_dir: str):
+    # Create the Docker client and build the Docker image
+    try:
+        docker = DockerClient(compose_files=[os.path.join(destination_dir, "docker-compose.yml")])
+        docker.compose.build(quiet=True)
+        docker.compose.up(detach=True, quiet=True)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred while creating the Docker client: {e}")
+
+    # Get the container ID and port
+    container_id = get_container_id(f"{submission_id}-app:latest")
+    port = get_container_port(container_id)
+
+    # Check if the application is running
+    flag = False
+    for _ in range(10):
+        try:
+            response = requests.get(f"http://localhost:{port}")
+            if response.status_code == 200:
+                flag = True
+                break
+        except Exception as e:
+            time.sleep(5)
+    if not flag:
+        raise HTTPException(status_code=500, detail="The application did not start successfully")
+    
+    # Send the submission to the application
+    response_json = response.json()
+    print(response_json)
+
+    # Stop the container and remove the volume
+    docker.compose.down(volumes=True, quiet=True)
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/create-submission")
 async def create_submission(
-        id: str = Form(...),
-        challenge_id: str = Form(...),
-        file: UploadFile = File(...)
-    ):
+    id: str = Form(...),
+    challenge_id: str = Form(...),
+    file: UploadFile = File(...),
+    background_tasks: BackgroundTasks = BackgroundTasks()
+):
     # Create the Submission instance from form data
     submission = Submission(id=id, challenge_id=challenge_id)
     submission_id = submission.id
     challenge_id = submission.challenge_id
     file_name = file.filename
-    
+
     # Set the source and destination directories
     source_dir = os.path.join("challenges", challenge_id)
     destination_dir = os.path.join("submissions", submission_id)
@@ -97,40 +133,11 @@ async def create_submission(
     else:
         raise HTTPException(status_code=404, detail=f"Replace File not found")
 
-    # Create the Docker client and build the Docker image
-    try:
-        docker = DockerClient(compose_files=[os.path.join(destination_dir, "docker-compose.yml")])
-        docker.compose.build(quiet=True)
-        docker.compose.up(detach=True, quiet=True)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while creating the Docker client: {e}")
+    # Schedule long-running tasks in background
+    background_tasks.add_task(
+        build_and_run_docker, challenge_id, submission_id, destination_dir
+    )
 
-    # Get the container ID and port
-    container_id = get_container_id(f"{submission_id}-app:latest")
-    port = get_container_port(container_id)
-
-    # Check if the application is running
-    flag = False
-    for _ in range(10):
-        try:
-            response = requests.get(f"http://localhost:{port}")
-            if response.status_code == 200:
-                flag = True
-                break
-        except Exception as e:
-            time.sleep(5)
-    if not flag:
-        raise HTTPException(status_code=500, detail="The application did not start successfully")
-    
-    # Send the submission to the application
-    response_json = response.json()
-
-    # Stop the container and remove the volume
-    docker.compose.down(volumes=True, quiet=True)
-    if os.path.exists(destination_dir):
-        shutil.rmtree(destination_dir)
-
-    return {"message": "Submission created", "port": port, "response": response_json}
-    # return {"message": "Submission created"}
+    return {"message": "Submission creation started"}
 
 # uvicorn app.main:app --port 8080
